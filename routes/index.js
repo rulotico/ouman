df53fa3d823c25aabfa6e8ead95bbe38da966070
rulotico <rulotico@gmail.com>
2015-10-10 19:22:30 -0500

    SEGUNDO COMMIT

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/vozrecord', function(req, res, next) {
+  res.render('voz', { title: 'Voz' });
+});
+
 module.exports = router;
