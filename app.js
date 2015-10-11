4124c258030244f60b0573b09660bea78546320e
rulotico <rulotico@gmail.com>
2015-10-11 01:36:06 -0500

    voice speech

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -58,3 +59,7 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+
+
+
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,30 @@
-main.js
+
+
+var recognizer = new webkitSpeechRecognition();
+recognizer.lang = "es-MX";
+
+
+$("#voice_btn").click(function (){
+
+$("#speech").html("");
+
+
+$(".redPoint").css("visibility","visible");
+
+
+
+recognizer.onresult = function(event) {
+    if (event.results.length > 0) {
+        var result = event.results[event.results.length-1];
+        if(result.isFinal) {
+            console.log(result[0].transcript);
+            $("#speech").html(result[0].transcript);
+            $(".redPoint").css("visibility","hidden");
+        }
+    }  
+};
+recognizer.start();
+
+});
+
+
